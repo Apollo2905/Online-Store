af466307f083b5660819ce6f18582bdb6431470c
Apollo2905 <polina.shashilo@mail.ru>
2023-08-21 20:57:48 +0300

    Created order_edit, views, forms

--- top/staff/forms.py
+++ top/staff/forms.py
@@ -2,4 +2,11 @@ from django import forms
 from .bulma_mixin import BulmaMixin
 from store.models import Order, STATUS
 
-#создайте форму
+
+class OrderEditForm(BulmaMixin, forms.ModelForm):
+    address = forms.CharField(label="Изменить адрес")
+    status = forms.ChoiceField(label="Изменить статус", choices=STATUS)
+
+    class Meta:
+        model = Order
+        fields = ['address', 'status']
--- top/staff/views.py
+++ top/staff/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from store.models import Order
+from .forms import OrderEditForm
 
 
 def home(request):
@@ -10,3 +11,17 @@ def home(request):
 def order(request, pk):
     order_data = Order.objects.get(pk=pk)
     return render(request, 'order.html', {'order': order_data})
+
+
+def order_edit(request, pk):
+    order_data = Order.objects.get(pk=pk)
+    form = OrderEditForm(request.POST or None, instance=order_data)
+    allowed_status = ['Создан', 'Принят', 'В сборке']
+
+    if not request.POST.get('address'):
+        form.address = order_data.address
+
+    if form.is_valid():
+        form.save()
+        return redirect('staff:order', pk=pk)
+    return render(request, 'order_edit.html', {'form': form, 'order': order_data, 'allowed_status': allowed_status})
